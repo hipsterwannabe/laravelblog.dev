90768cb18a8709acb5eba5d31b809f7f4c3de4b4
Greg Vallejo <greg.vallejo@gmail.com>
2014-06-26 11:23:35 -0500

    added new routes to blog site for resume and portfolio

--- app/routes.php
+++ app/routes.php
@@ -10,8 +10,28 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
+// sample routes
+// Route::get('/', function(){
+// 	return 'we are home';
+// });
 
-Route::get('/', function()
-{
-	return View::make('hello');
+// Route::get('/sayHello/{name}', function($name)
+// {
+// 	 if ($name == "Greg")
+//     {
+//         return Redirect::to('/');
+//     }
+//     else
+//     {
+//         return "Hello, $name!";
+//     }
+// });
+
+Route::get('/resume', function(){
+	return 'resume';
 });
+
+Route::get('/portfolio', function(){
+	return 'portfolio';
+});
+
