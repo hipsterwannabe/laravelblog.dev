b2393c1dea914ee5b6f0936a423038916d9cd8f8
Greg Vallejo <greg.vallejo@gmail.com>
2014-06-26 14:12:32 -0500

    rolldice route, need to fix logic and view

--- app/routes.php
+++ app/routes.php
@@ -12,7 +12,7 @@
 */
 // sample routes
 Route::get('/', function(){
-	return 'we are home';
+	return View::make('temp.my-first-view');
 });
 
 // Route::get('/sayHello/{name}', function($name)
@@ -35,3 +35,31 @@ Route::get('/portfolio', function(){
 	return 'portfolio';
 });
 
+Route::get('/sayhello/{name}', function($name)
+{
+    if ($name === "Greg")
+    {
+        return Redirect::to('/');
+    }
+    else
+    {
+        return View::make('my-first-view')->with('name', $name);
+    }
+});
+
+
+// Create a route that responds to a GET request on the path /rolldice.
+// Within the route, return a random number between 1 and 6.
+// Add a view named roll-dice.php. Instead of just returning the random number, show the view and have it display the random number.
+// Modify the route to take in a parameter named guess. Someone will access the route by visiting http://blog.dev/rolldice/1, where 1 is their guess.
+// Modify the route and view so that you can display the guess in addition to the roll and also tell if the guess matches the roll.
+
+Route::get('/rolldice/{guess}', function($guess){
+	$roll = rand(1,6);
+	if ($guess == $roll){
+		return 'Your guess matched the roll!';
+	} else {
+		return 'You guessed wrong!';
+	}
+		
+});
